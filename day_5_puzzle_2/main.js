9fe9c0a1a0c0d87f0cbac31562af5e7d2c6dcf08
Heijnen,Dirk D.J.C <djc.heijnen@student.fontys.nl>
2023-12-05 21:42:37 +0100

    Added day 5 of advent of code

---
+++ day_5_puzzle_2/main.js
@@ -0,0 +1,165 @@
+const fs = require("fs");
+const path = require("path");
+
+
+class TransformationMap {
+
+    constructor() {
+        this.entries = [];
+    }
+
+    addEntry(destinationRangeStart, sourceRangeStart, rangeLength) {
+        this.entries.push({
+            sourceRangeStart: sourceRangeStart,
+            destinationRangeStart: destinationRangeStart,
+            rangeLength: rangeLength
+        });
+    }
+
+    getDestination(source) {
+        for (let entry of this.entries) {
+
+            const sourceRangeStart = entry.sourceRangeStart;
+            const sourceRangeEnd = entry.sourceRangeStart + (entry.rangeLength - 1);
+
+            if (source >= sourceRangeStart && source <= sourceRangeEnd) {
+
+                // Get the difference between the input source and the source range start.
+                let difference = Math.abs(source - entry.sourceRangeStart);
+            
+                const destination = entry.destinationRangeStart + difference;
+                return destination;
+            }
+        }
+
+        return source;
+    }
+}
+
+
+class PipelineStep {
+
+    constructor(map) {
+        this.map = map;
+    }
+
+    run(input) {
+        return this.map.getDestination(input);
+    }
+}
+
+
+class Pipeline {
+
+    constructor() {
+        this.steps = [];
+    }
+
+    addStep(step) {
+        this.steps.push(step);
+    }
+
+    run(input) {
+        let previousStepOutput = input;
+
+        for (let step of this.steps) {
+            previousStepOutput = step.run(previousStepOutput);
+        }
+
+        return previousStepOutput;
+    }
+}
+
+
+class Almanac {
+
+    constructor(text) {
+        this.seedRanges = [];
+        this.transformationMaps = [];
+
+        // 1. Take all the lines.
+        let textLines = text.split('\r\n');
+
+        // 2. Extract the seeds
+        let seedNumbers = textLines[0].replace('seeds: ', '').split(' ').map(x => parseInt(x));
+        while (seedNumbers.length > 0) {
+            const seedRangeStart = seedNumbers.shift();
+            const seedRangeLength = seedNumbers.shift();
+            const seedRangeEnd = seedRangeStart + seedRangeLength;
+            this.seedRanges.push({ start: seedRangeStart, end: seedRangeEnd});
+        }
+
+        // 3. Remove the first two lines, they are the seed line and an empty line.
+        textLines.shift();
+        textLines.shift();
+
+        // 4. Split the textLines into subarrays, each forming a transformation map.
+        let transformationMap = new TransformationMap();
+
+        for (let i = 0; i < textLines.length; i++) {
+            // If the line is empty, complete the current map and move on.
+            if (textLines[i] === '') {
+                this.transformationMaps.push(transformationMap)
+                transformationMap = new TransformationMap();
+                continue;
+            }
+
+            // Otherwise, check if the line contains the text 'map', if that is the case, skip the line.
+            if (textLines[i].includes('map')) {
+                continue;
+            }
+
+            // The line contains an transformation map entry.
+            let transformationMapEntryArray = textLines[i].split(' ').map(x => parseInt(x));
+            transformationMap.addEntry(
+                transformationMapEntryArray[0], 
+                transformationMapEntryArray[1], 
+                transformationMapEntryArray[2]
+            );
+
+            // If it's the final row of the final transformation map, the loop won't be executed anymore.
+            // This means that we need to appennd the final transformation map this way.
+            if (i === textLines.length - 1) {
+                this.transformationMaps.push(transformationMap)
+            }
+        }
+    }
+}
+
+
+const main = () => {
+    // Read the input file.
+    const inputFilePath = path.join(__dirname, 'input.txt');
+    const inputFileText = fs.readFileSync(inputFilePath, 'utf-8');
+
+
+    // Parse the Almanac.
+    const almanac = new Almanac(inputFileText);
+
+    // Create the pipeline.
+    const pipeline = new Pipeline();
+
+    // For each map in the almanac, add a pipeline step with the corresponding map.
+    for (let map of almanac.transformationMaps) {
+        pipeline.addStep(new PipelineStep(map));
+    }
+
+    // Define the location value for the lowest detected location.
+    let location = Number.MAX_SAFE_INTEGER;
+
+    for (let seedRange of almanac.seedRanges) {
+        for (let i = seedRange.start; i < seedRange.end; i++) {
+            let newLocation = pipeline.run(i);
+
+            if (newLocation < location) {
+                location = newLocation;
+            }
+        }
+    }
+
+    // Log the answer.
+    console.log("The closest location = " + location);
+}
+
+
+main();
